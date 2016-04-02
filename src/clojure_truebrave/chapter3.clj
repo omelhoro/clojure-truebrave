4dbe6a0f31446e1ddfdbc18176d5a122b31b9ae1
omelhoro <igor.fischer154@gmail.com>
2016-04-02 19:01:46 +0200

    More tests for chapter3

--- src/clojure_truebrave/chapter3.clj
+++ src/clojure_truebrave/chapter3.clj
@@ -50,4 +50,31 @@
 
 (hit [{:size 2 :name "eye"} {:size 3 :name "head"} {:size 1 :name "feet"}] 2)
 
+(str "Igor" " the" " Best")
 
+(vector 1 2 '(1 2 3 4) '(5 6 6))
+
+(list 1 23 4)
+
+(:a (hash-map  :a 2 :b 3))
+
+(hash-set 1 2 1 2)
+
+(defn add100
+  "This function add 100 to a number"
+  [n]
+  (+ n 100))
+
+(defn dec-maker
+  "Returns a fn that decrements a number"
+  [n]
+  #(- % n))
+
+(defn mapset
+  "Works like map but returns a set"
+  [f col]
+  (-> (map f col) set))
+
+(defn symmetrize-n
+  ([parts] (symmetrize-n parts 2))
+  ([parts n] (symmetrize parts)))
--- test/clojure_truebrave/chapter3_test.clj
+++ test/clojure_truebrave/chapter3_test.clj
@@ -2,34 +2,30 @@
   (:require
    [clojure.test :refer :all]
    clojure.data
-   [clojure-truebrave.chapter3 :refer :all]
-   ))
+   [clojure-truebrave.chapter3 :refer :all]))
 
-(def test-body-unsym [
-                      {:name "left-arm" :size 4}
-                      {:name "head" :size 1}
-                      ])
-(def test-body-sym [
-                    {:name "left-arm" :size 4}
+(def test-body-unsym [{:name "left-arm" :size 4}
+                      {:name "head" :size 1}])
+(def test-body-sym [{:name "left-arm" :size 4}
                     {:name "right-arm" :size 4}
-                    {:name "head" :size 1}
-                    ])
+                    {:name "head" :size 1}])
 
 (deftest symmetric-test
   (testing "should give a symmetric person"
-    (let [
-          in test-body-unsym
-          out test-body-sym
-          ]
+    (let [in test-body-unsym
+          out test-body-sym]
       (is (= (symmetrize in) out)))))
 
 (deftest my-reduce-test
   (testing "If my reduce works"
     (is (= 3 (my-reduce + [1 2]))))
   (testing "If my reduce works with initial values"
-    (is (= 3 (my-reduce + 0 [1 2]))))
-  )
+    (is (= 3 (my-reduce + 0 [1 2])))))
 
+(deftest my-reduce-test
+  (testing "If my reduce works"
+    (are [x y a] (= x (my-reduce y a))
+      3 + [1 2])))
 
 (deftest hit-test
   (testing "Should hit a part of body"
@@ -37,5 +33,34 @@
   (testing "Should hit nothing"
     (is (= (hit test-body-sym 0) nil)))
   (testing "Should hit head"
-    (is (= (hit test-body-sym 4) (first test-body-sym))))
-  )
+    (is (= (hit test-body-sym 4) (first test-body-sym)))))
+
+(deftest add100-test
+  (testing "Should add 100"
+    (are [x y] (= x (add100 y))
+      200 100
+      400 300
+      101 1)))
+
+(deftest dec-maker-test
+  (testing "Should create a fn to decrement something"
+    (are [x y z] (= x (y z))
+      5 (dec-maker 5) 10
+      12 (dec-maker 2) 14
+      3 (dec-maker 40) 43)
+    ))
+
+(deftest mapset-test
+  (testing "Mapset should work"
+    (are [x y f] (= x (mapset f y))
+      #{1 2 3} [0 1 0 2] inc
+      #{2 3 4} [3 3 3 4 4 5] dec)))
+
+(deftest symmetric-n-test
+  (testing "Test should verify that n parts are symmetrized"
+    (are [x y] (= y (symmetrize-n x))
+      [{:name "right-ear"}] [{:name "right-ear"} {:name "left-ear"}])
+    (are [x y n] (= y (symmetrize-n x n))
+      [{:name "right-ear"} {:name "right-ear"}]
+      [{:name "right-ear"} {:name "left-ear"} {:name "right-ear"} {:name "left-ear"}]
+      4)))
